92a6709f7e94036ea70bef61021395ac7de4f97a
Mdhossain60 <110592017+Mdhossain60@users.noreply.github.com>
2022-11-07 00:13:10 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+ $(function (){
+
+        let searchIcon = $('.searchIcon')
+         let croseIcon = $('.croseIcon')
+         let searchArea =  $('.searchArea')
+
+  searchIcon.click(function(){
+    searchArea .slideDown(300)
+  })
+
+  croseIcon.click(function(){
+    searchArea .slideUp(300)
+  })
+
+
+
+
+
+
+
+ })
